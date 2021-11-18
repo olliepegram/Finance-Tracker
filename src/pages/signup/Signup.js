ea27371f61e7ee4e75007d0db21ec120c9a6709c
Ollie Pegram <olliepegram@gmail.com>
2021-11-18 18:31:01 +1100

    signup form implemented

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,45 @@
+import { useState } from 'react';
 import styles from './Signup.module.css';
 
 export default function Signup() {
-    return <div>Signup</div>;
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(username, email, password);
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className={styles['signup-form']}>
+            <h2>Signup</h2>
+            <label>
+                <span>username:</span>
+                <input
+                    type='text'
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                />
+            </label>
+            <label>
+                <span>email:</span>
+                <input
+                    type='email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+            <label>
+                <span>password:</span>
+                <input
+                    type='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className='btn'>Signup</button>'
+        </form>
+    );
 }
